e59688a9b7fc88567ba2f2d76d0a642348440f56
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-06-14 19:02:55 +0200

    Gravity flip codewars

--- src/main/java/gravityFlip/Kata.java
+++ src/main/java/gravityFlip/Kata.java
@@ -1,7 +1,23 @@
 package gravityFlip;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Kata {
     public static int[] flip(char dir, int[] arr) {
+        if (dir == 'L') {
+            arr =
+                    Arrays.stream(arr)
+                            .boxed()
+                            .sorted(Collections.reverseOrder())
+                            .mapToInt(Integer::intValue)
+                            .toArray();
+        }
+        else {
+            arr = Arrays.stream(arr)
+                    .sorted()
+                    .toArray();
+        }
         return arr;
     }
 }
