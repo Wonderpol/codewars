5f39e3632402edf73f2a023310cb5a68869342eb
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-06-15 20:42:46 +0200

    Easy logs kata

--- src/main/java/sumOfDifferencesInArray/ZywOo.java
+++ src/main/java/sumOfDifferencesInArray/ZywOo.java
@@ -1,2 +1,20 @@
-package sumOfDifferencesInArray;public class ZywOo {
+package sumOfDifferencesInArray;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+
+public class ZywOo {
+    public static int sumOfDifferences(int[] arr) {
+        arr = Arrays.stream(arr)
+                .boxed()
+                .sorted(Collections.reverseOrder())
+                .mapToInt(Integer::intValue)
+                .toArray();
+        int sum = 0;
+        for(int i = 0; i < arr.length - 1; i++) {
+            sum += (arr[i] - arr[i + 1]);
+        }
+        return sum;
+    }
 }
