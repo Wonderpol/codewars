ae4726181d44db1d2c9ac69da613dc0f2bc4fec5
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-06-17 20:46:31 +0200

    Area or parimeter v2

--- src/main/java/areaOrPerimeter/Solution.java
+++ src/main/java/areaOrPerimeter/Solution.java
@@ -2,9 +2,6 @@ package areaOrPerimeter;
 
 public class Solution {
     public static int areaOrPerimeter (int l, int w) {
-        if (l == w) {
-            return l*w;
-        }
-        return 2 * l + 2 * w;
+        return l == w ? l * w : 2 * w + 2 * l;
     }
 }
