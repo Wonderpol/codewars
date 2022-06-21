9fdc3881a2627963c85e89ccc8b7ee84b545da82
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-06-21 23:26:09 +0200

    Last survivor

--- src/main/java/lastSurvivor/Kata.java
+++ src/main/java/lastSurvivor/Kata.java
@@ -4,10 +4,12 @@ import java.util.Arrays;
 
 public class Kata {
     public static String lastSurvivor(String letters, int[] coords) {
-        for (int i = 0; i < coords.length; i++) {
-            letters = letters.substring(coords[i]);
-            coords = Arrays.stream(coords).boxed().mapToInt(n -> n - 1).toArray();
-        }
-        return letters;
+        StringBuilder stringBuilder = new StringBuilder(letters);
+
+        Arrays.stream(coords)
+                .boxed()
+                .forEach(stringBuilder::deleteCharAt);
+
+        return stringBuilder.toString();
     }
 }
