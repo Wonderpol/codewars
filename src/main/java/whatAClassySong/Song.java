28cdff5ac140dfc79bedc6db75e6258dd22dcc86
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-06-21 23:48:15 +0200

    What a classy Song

---
+++ src/main/java/whatAClassySong/Song.java
@@ -0,0 +1,25 @@
+package whatAClassySong;
+import java.util.ArrayList;
+
+public class Song {
+    private final String title;
+    private final String artist;
+
+    Song(final String title, final String artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    String getTitle() {
+        return title;
+    }
+
+    String getArtist() {
+        return artist;
+    }
+
+    public int howMany(ArrayList<?> list) {
+        return list.size();
+    }
+
+}
