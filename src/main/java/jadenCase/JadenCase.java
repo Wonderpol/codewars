ef8dea572d8cacb982c16ee99b15ef857a05213d
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-06-28 23:24:03 +0200

    JadenCase

--- src/main/java/jadenCase/JadenCase.java
+++ src/main/java/jadenCase/JadenCase.java
@@ -1,2 +1,25 @@
-package jadenCase;public class JadenCase {
+package jadenCase;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class JadenCase {
+    public static String toJadenCase(String phrase) {
+        if (phrase != null && !phrase.isBlank()) {
+            String[] phraseWords = phrase.split(" ");
+
+           return Arrays.stream(phraseWords)
+                    .map(s -> s.isEmpty() ? "" : s.substring(0, 1).toUpperCase() + s.substring(1))
+                    .collect(Collectors.joining(" "));
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(toJadenCase("jan ma kota"));
+        System.out.println(toJadenCase(null));
+    }
 }
--- src/test/java/jadenCase/JadenCaseTest.java
+++ src/test/java/jadenCase/JadenCaseTest.java
@@ -1,4 +1,24 @@
+package jadenCase;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class JadenCaseTest {
-  
+    JadenCase jadenCase = new JadenCase();
+
+    @Test
+    public void test() {
+        assertEquals("Most Trees Are Blue", jadenCase.toJadenCase("most trees are blue"));
+    }
+
+    @Test
+    public void testNullArg() {
+        assertNull("Must return null when the arg is null", jadenCase.toJadenCase(null));
+    }
+
+    @Test
+    public void testEmptyArg() {
+        assertNull("Must return null when the arg is empty string", jadenCase.toJadenCase(""));
+    }
 }
