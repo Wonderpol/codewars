d957bc4342f8da01d1d59e9458bcc6c30819f950
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-07-02 22:12:38 +0200

    Partlist

---
+++ src/main/java/Partlist/Partlist.java
@@ -0,0 +1,26 @@
+package Partlist;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Partlist {
+    static String[][] partlist(final String[] arr) {
+
+        List<String> copiedArray = Arrays.asList(arr);
+        List<List<String>> result = new ArrayList<>();
+
+        for (int i = 1; i < arr.length; i++) {
+            List<String> arrayResult = new ArrayList<>();
+            arrayResult.add(String.join(" ", copiedArray.subList(0, i)));
+            arrayResult.add(String.join(" ", copiedArray.subList(i, arr.length)));
+
+            result.add(arrayResult);
+        }
+
+        return result.stream()
+                .map(lst -> lst.toArray(String[]::new))
+                .toArray(String[][]::new);
+    }
+
+}
