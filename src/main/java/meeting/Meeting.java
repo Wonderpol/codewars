162dbfdca36a893e9aaafca15aae7d6a2a71d7ad
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-08-14 23:39:35 +0200

    Meeting kata kyu6 final version

--- src/main/java/meeting/Meeting.java
+++ src/main/java/meeting/Meeting.java
@@ -6,20 +6,6 @@ import java.util.Collections;
 import java.util.List;
 
 public class Meeting {
-//    public static String meeting(String s) {
-//
-//
-//        String[] splittedData = s.replaceAll(":", ", ").split(";");
-//        String[] result = new String[splittedData.length];
-//
-//        for (int i = 0; i < splittedData.length; i++) {
-//            result[i] = "(" + splittedData[i].toUpperCase() + ")";
-//        }
-//
-//        Arrays.sort(result);
-//
-//        return String.join("", result);
-//    }
 
     public static String meeting(String s) {
         String[] guests = s.split(";");
@@ -30,9 +16,5 @@ public class Meeting {
         Arrays.sort(guests);
 
         return String.join("",guests);
-    }
-
-    public static void main(String[] args) {
-        System.out.println(meeting("Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill"));
     }
 }
