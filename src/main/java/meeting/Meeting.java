07b6b07faae73abebb0c02fe5ffdf4bbfbf8a98f
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-08-14 23:35:36 +0200

    Meeting kata kyu6

--- src/main/java/meeting/Meeting.java
+++ src/main/java/meeting/Meeting.java
@@ -1,16 +1,35 @@
 package meeting;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Meeting {
+//    public static String meeting(String s) {
+//
+//
+//        String[] splittedData = s.replaceAll(":", ", ").split(";");
+//        String[] result = new String[splittedData.length];
+//
+//        for (int i = 0; i < splittedData.length; i++) {
+//            result[i] = "(" + splittedData[i].toUpperCase() + ")";
+//        }
+//
+//        Arrays.sort(result);
+//
+//        return String.join("", result);
+//    }
+
     public static String meeting(String s) {
-        final StringBuilder result = new StringBuilder();
-
-        String[] splittedData = s.replaceAll(":", ", ").split(";");
-
-        for (String meeting: splittedData) {
-            result.append("(").append(meeting).append(")");
+        String[] guests = s.split(";");
+        for (int i = 0; i < guests.length; i++){
+            int index = guests[i].indexOf(":");
+            guests[i] = "(" + guests[i].substring(index+1).toUpperCase() + ", " + guests[i].substring(0,index).toUpperCase() + ")";
         }
+        Arrays.sort(guests);
 
-        return result.toString();
+        return String.join("",guests);
     }
 
     public static void main(String[] args) {
