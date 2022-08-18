321d9f6eb1c4f669237495c45a779acab6f923ad
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-08-18 22:27:02 +0200

    Your order please kata kyu6

--- src/main/java/meeting/Meeting.java
+++ src/main/java/meeting/Meeting.java
@@ -1,9 +1,7 @@
 package meeting;
 
-import java.util.ArrayList;
+
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class Meeting {
 
---
+++ src/main/java/yourOrderPlease/Order.java
@@ -0,0 +1,36 @@
+package yourOrderPlease;
+
+import java.util.*;
+
+import static java.util.Map.Entry.comparingByKey;
+
+public class Order {
+    public static String order(String words) {
+        String[] splittedWords = words.split(" ");
+
+        Map<Integer, String> resultMap = new LinkedHashMap<>();
+
+        for (String word : splittedWords) {
+            char[] wordChars = word.toCharArray();
+            for (char c : wordChars) {
+                if (Character.isDigit(c)) {
+                    resultMap.put(Character.getNumericValue(c), word);
+                }
+            }
+        }
+
+        final StringBuilder result = new StringBuilder();
+
+        resultMap
+                .entrySet()
+                .stream()
+                .sorted(comparingByKey())
+                .forEach(entry -> result.append(entry.getValue()).append(" "));
+
+        return result.toString().trim();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(order("Thi1s Jac4ob moth3er fat2her"));
+    }
+}
