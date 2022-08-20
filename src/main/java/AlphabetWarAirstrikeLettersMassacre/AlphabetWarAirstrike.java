b726c1a4e7ee414c4ce4cf8b1066854e1b435408
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-08-20 17:44:16 +0200

    Alphabet airstrike kata kyu6 not fidnished yet

---
+++ src/main/java/AlphabetWarAirstrikeLettersMassacre/AlphabetWarAirstrike.java
@@ -0,0 +1,10 @@
+package AlphabetWarAirstrikeLettersMassacre;
+
+public class AlphabetWarAirstrike {
+    public static String alphabetWar(String fight){
+
+        //TODO: No idea how to work with this kata
+
+        return "Have mercy!";
+    }
+}
