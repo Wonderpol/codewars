ad63e1a91d94abf810973cc59d3e63ba36efa3a8
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-08-21 20:53:20 +0200

    Mexican wave kyu6

--- src/main/java/mexicanWave/MexicanWave.java
+++ src/main/java/mexicanWave/MexicanWave.java
@@ -1,8 +1,26 @@
 package mexicanWave;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MexicanWave {
     public static String[] wave(String str) {
-        // Your code here
+
+        char[] strArr = str.toCharArray();
+
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < strArr.length; i++) {
+
+            String word = str.replace(str.charAt(i), Character.toUpperCase(strArr[i]));
+
+            result.add(word);
+        }
+
         return new String[] {};
+    }
+
+    public static void main(String[] args) {
+        wave("hello");
     }
 }
