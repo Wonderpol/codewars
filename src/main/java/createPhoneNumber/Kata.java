b3de75a6aef6a8d1c08ba15cc7c13cb43ec9774c
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-08-23 18:35:24 +0200

    Create phone number kata kuy6

--- src/main/java/createPhoneNumber/Kata.java
+++ src/main/java/createPhoneNumber/Kata.java
@@ -18,8 +18,5 @@ public class Kata {
 
         return result.toString();
     }
-
-    public static void main(String[] args) {
-        System.out.println(Kata.createPhoneNumber(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
-    }
+    
 }
