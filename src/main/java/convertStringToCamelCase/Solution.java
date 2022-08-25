3eec84fcfd8577b927d9d1170d58c36d06141a79
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-08-25 14:33:33 +0200

    Convert string to camel case kata kyu6

--- src/main/java/convertStringToCamelCase/Solution.java
+++ src/main/java/convertStringToCamelCase/Solution.java
@@ -15,8 +15,4 @@ public class Solution {
 
         return result.toString();
     }
-
-    public static void main(String[] args) {
-        System.out.println(toCamelCase("the_Stealth_Warrior"));
-    }
 }
