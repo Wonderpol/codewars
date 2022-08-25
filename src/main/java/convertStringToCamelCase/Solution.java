c7b85a334ff618ab32c0d3e0482416de367551dd
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-08-25 14:32:28 +0200

    Convert string to camel case kata kyu6

--- src/main/java/convertStringToCamelCase/Solution.java
+++ src/main/java/convertStringToCamelCase/Solution.java
@@ -1,7 +1,22 @@
 package convertStringToCamelCase;
 
+import java.util.Arrays;
+
 public class Solution {
     static String toCamelCase(String s){
-        return "";
+
+        final StringBuilder result = new StringBuilder();
+        String[] splittedS = s.replaceAll("_", " ").replaceAll("-", " ").split(" ");
+
+        result.append(splittedS[0]);
+        Arrays.stream(splittedS)
+                .skip(1)
+                .forEach(str -> result.append(str.substring(0, 1).toUpperCase()).append(str.substring(1)));
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(toCamelCase("the_Stealth_Warrior"));
     }
 }
