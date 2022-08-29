279ec5aa1ef49c1cd8bd3f28227f989daf8c9b91
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-08-29 22:38:18 +0200

    Make the Deadfish swim kyu 6 with main for tests

--- src/main/java/makeTheDeadfishSwim/DeadFish.java
+++ src/main/java/makeTheDeadfishSwim/DeadFish.java
@@ -1,4 +1,41 @@
 package makeTheDeadfishSwim;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class DeadFish {
+    public static int[] parse(String data) {
+        char[] dataChArr = data.toCharArray();
+
+        int value = 0;
+        List<Integer> result = new ArrayList<>();
+
+        for (final char c : dataChArr) {
+            switch (c) {
+                case 'i':
+                    value++;
+                    break;
+                case 'd':
+                    value--;
+                    break;
+                case 's':
+                    value *= value;
+                    break;
+                case 'o':
+                    result.add(value);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return result.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(parse("iiisdosos")));
+    }
 }
