b9e174f7403cb983f6c959a6ba687af95bf736c3
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-08-31 21:39:50 +0200

    Highest scoring word kyu 6 - not every test passes

--- src/main/java/highestScoringWord/Kata.java
+++ src/main/java/highestScoringWord/Kata.java
@@ -1,6 +1,7 @@
 package highestScoringWord;
 
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -9,7 +10,7 @@ public class Kata {
     public static String high(String s) {
 
         String[] words = s.split(" ");
-        Map<Integer, String> result = new LinkedHashMap<>();
+        Map<Integer, String> result = new HashMap<>();
 
         for (final String word : words) {
             int score = word
@@ -18,7 +19,16 @@ public class Kata {
                     .map(integer -> integer - 96)
                     .mapToInt(Integer::intValue)
                     .sum();
+
+                if (result.isEmpty()) {
+                    result.put(score, word);
+                    continue;
+                }
+
+                if (result.containsKey(score)) continue;
+
             result.put(score, word);
+
         }
 
         return result.entrySet()
