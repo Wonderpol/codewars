d0c72ba1f6736bbe34e2cc6eca1e898b456e5069
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-09-04 18:51:24 +0200

    Kata for further solve

---
+++ src/main/java/takeATenMinutesWalk/TenMinWalk.java
@@ -0,0 +1,10 @@
+package takeATenMinutesWalk;
+
+public class TenMinWalk {
+    public static boolean isValid(char[] walk) {
+
+        //TODO: Don't understand this kata
+
+        return true;
+    }
+}
