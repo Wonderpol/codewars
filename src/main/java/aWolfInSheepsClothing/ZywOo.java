83d9da6e46022b255478ffd345fe081f645072c8
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-09-13 22:20:50 +0200

    Reformatted old code

--- src/main/java/aWolfInSheepsClothing/ZywOo.java
+++ src/main/java/aWolfInSheepsClothing/ZywOo.java
@@ -7,7 +7,11 @@ public class ZywOo {
         if (array[array.length - 1].equals("wolf")){
             return "Pls go away and stop eating my sheep";
         }
+
         int indexOfWolf = Arrays.asList(array).indexOf("wolf");
-        return "Oi! Sheep number "+ (array.length - 1 - indexOfWolf) +"! You are about to be eaten by a wolf!";
+
+        return "Oi! Sheep number "
+                + (array.length - 1 - indexOfWolf)
+                +"! You are about to be eaten by a wolf!";
     }
 }
