8aad0382b347c257ab4b5d7e7bcb6707e1a59be3
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-09-22 19:02:09 +0200

    Who likes it kata kyu6 refactored to more clever version using switch

--- src/main/java/whoLikesIt/Solution.java
+++ src/main/java/whoLikesIt/Solution.java
@@ -4,25 +4,13 @@ import java.util.Arrays;
 
 public class Solution {
     public static String whoLikesIt(String ... names) {
-        if (names.length == 0) return "no one likes this";
-        final StringBuilder result = new StringBuilder();
-
-        if (names.length == 1) result.append(names[0]);
-        
-        if (names.length == 2) {
-            result.append(names[0]);
-            Arrays.stream(names)
-                    .skip(1)
-                    .forEach(name -> result.append(" and ").append(name));
-        } else if (names.length == 3) {
-            result.append(names[0]).append(", ");
-            result.append(names[1]).append(" and ").append(names[2]);
-        } else if (names.length > 3) {
-            result.append(names[0]).append(", ").append(names[1]);
-            result.append(" and ").append(names.length - 2).append(" others");
+        switch (names.length) {
+            case 0: return "no one likes this";
+            case 1: return String.format("%s likes this", names[0]);
+            case 2: return String.format("%s and %s like this", names[0], names[1]);
+            case 3: return String.format("%s, %s and %s like this", names[0], names[1], names[2]);
+            default: return String.format("%s, %s and %d others like this", names[0], names[1], names.length - 2);
         }
-
-        return result.append(names.length > 1 ? " like this" : " likes this").toString();
     }
 
     public static void main(String[] args) {
