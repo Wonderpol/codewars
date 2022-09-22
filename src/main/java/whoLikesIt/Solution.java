932da2725647da8584d6688351f93f454ce355ec
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-09-22 18:58:40 +0200

    Who likes it kata kyu6

--- src/main/java/whoLikesIt/Solution.java
+++ src/main/java/whoLikesIt/Solution.java
@@ -7,11 +7,19 @@ public class Solution {
         if (names.length == 0) return "no one likes this";
         final StringBuilder result = new StringBuilder();
 
-        if (names.length < 4) {
+        if (names.length == 1) result.append(names[0]);
+        
+        if (names.length == 2) {
             result.append(names[0]);
             Arrays.stream(names)
                     .skip(1)
                     .forEach(name -> result.append(" and ").append(name));
+        } else if (names.length == 3) {
+            result.append(names[0]).append(", ");
+            result.append(names[1]).append(" and ").append(names[2]);
+        } else if (names.length > 3) {
+            result.append(names[0]).append(", ").append(names[1]);
+            result.append(" and ").append(names.length - 2).append(" others");
         }
 
         return result.append(names.length > 1 ? " like this" : " likes this").toString();
