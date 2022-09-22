cc969c0310ca85c25e14ef21c26a20b51f25ff2f
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-09-22 18:52:57 +0200

    Who likes it started with edge cases

--- src/main/java/whoLikesIt/Solution.java
+++ src/main/java/whoLikesIt/Solution.java
@@ -1,15 +1,23 @@
 package whoLikesIt;
 
+import java.util.Arrays;
+
 public class Solution {
     public static String whoLikesIt(String ... names) {
+        if (names.length == 0) return "no one likes this";
         final StringBuilder result = new StringBuilder();
 
-        if (names.length == 0) return "no one likes this";
+        if (names.length < 4) {
+            result.append(names[0]);
+            Arrays.stream(names)
+                    .skip(1)
+                    .forEach(name -> result.append(" and ").append(name));
+        }
 
-        return result.toString();
+        return result.append(names.length > 1 ? " like this" : " likes this").toString();
     }
 
     public static void main(String[] args) {
-
+        System.out.println(whoLikesIt("Jan", "Adam"));
     }
 }
