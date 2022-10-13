8d3a761e135cb4d45d92f62fe7940176ad7313ba
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-10-13 12:04:50 +0200

    Jetbrains task

---
+++ src/main/java/hashCodeAndEquals/BlackBox.java
@@ -0,0 +1,58 @@
+package hashCodeAndEquals;
+
+public class BlackBox {
+
+    private final int additive;
+    private int input1;
+    private int input2;
+
+    public BlackBox(int add){
+        additive = add;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    public void updateIns(int in1, int in2){
+        input1 = in1;
+        input2 = in2;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return  true;
+
+        if (!(obj instanceof BlackBox)) return false;
+
+        BlackBox that = (BlackBox) obj;
+
+        return additive == that.additive
+                &&
+                input1 == that.input1
+                &&
+                input2 == that.input2;
+    }
+
+
+    public static void main(String[] args) {
+
+        BlackBox bb1 = new BlackBox(13);
+        BlackBox bb2 = new BlackBox(13);
+        Object bb3 = new BlackBox(13);
+
+        System.out.print(bb2.equals(bb3) + " ");
+
+        bb1.updateIns(1, 4);
+
+        System.out.print(bb1.equals(bb1) + " ");
+
+        System.out.print(bb1.equals(bb3) + " ");
+
+        bb2.updateIns(1, 4);
+
+        System.out.print(bb2.equals(bb1));
+    }
+}
---
+++ src/main/java/hashCodeAndEquals/ComplexNumber.java
@@ -0,0 +1,35 @@
+package hashCodeAndEquals;
+
+import java.util.Objects;
+
+class ComplexNumber {
+
+    private final double re;
+    private final double im;
+
+    public ComplexNumber(double re, double im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    public double getRe() {
+        return re;
+    }
+
+    public double getIm() {
+        return im;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final ComplexNumber that = (ComplexNumber) o;
+        return Double.compare(that.re, re) == 0 && Double.compare(that.im, im) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(re, im);
+    }
+}
